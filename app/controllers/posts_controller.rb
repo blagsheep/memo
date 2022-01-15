5c28a3670c8b0730a4ad848f5547d9227c842d80
Memo <marcus.forchner@gmail.com>
2022-01-15 18:26:41 +0100

    use static images with custom markers for artice map images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user! , only: [:index, :edit, :new, :create, :update, :destroy]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :require_permission, only: [:edit, :update, :destroy]
   
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,26 @@ class Post < ApplicationRecord
   has_rich_text :body
   has_one_attached :photo
 
+
+  def map_with_pins
+
+    map_url = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/"
+    map_url_end = "/auto/1024x576@2x?access_token=pk.eyJ1IjoibWFyY3VzLWZvcmNobmVyIiwiYSI6ImNreDA1MHI2czBkMXUycXFsd2JqcWVtZWoifQ.LUGuOxwCmOtjcghKK-dDRg&logo=false"
+    counter = 0
+    self.body.embeds.each do |embedded_image|
+      counter = counter + 1
+      if !embedded_image.metadata["longitude"].nil?   
+        map_url = map_url + "url-" + CGI.escape(embedded_image.variant(resize_to_limit: [150, 100]).processed.service_url) + "(" + embedded_image.metadata["longitude"].to_s + "," + embedded_image.metadata["latitude"].to_s + ")"
+        if counter < body.embeds.size 
+          map_url = map_url + ","
+        end
+      end
+    end
+
+    return map_url + map_url_end
+  end
+
 end
+
+
+
