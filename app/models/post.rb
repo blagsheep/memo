77c635420b97e309a151ada08c2c086b63fe6e04
Memo <marcus.forchner@gmail.com>
2022-01-23 16:30:28 +0100

    add article previews on home

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of   :title, :body, :user_id
+  validates_presence_of   :title, :body, :summary, :user_id
   has_rich_text :body
   has_one_attached :photo
 
@@ -15,6 +15,7 @@ class Post < ApplicationRecord
       all_longitudes = []
       all_latitudes = []
 
+      # collect all lat, long values of all attachments
       self.body.embeds.each do |embedded_image|
         if !embedded_image.metadata["longitude"].nil?
           all_longitudes.append(embedded_image.metadata["longitude"])
@@ -23,8 +24,17 @@ class Post < ApplicationRecord
       end
 
       map_url = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/"
-      map_url_end = 
+      
+
+      - if body.embeds.size > 1
+        # use bounding box
+        map_url_end = 
         "/["+all_longitudes.min.to_s+","+all_latitudes.min.to_s+","+all_longitudes.max.to_s+","+all_latitudes.max.to_s+"]/1024x576@2x?padding=50&access_token=pk.eyJ1IjoibWFyY3VzLWZvcmNobmVyIiwiYSI6ImNreDA1MHI2czBkMXUycXFsd2JqcWVtZWoifQ.LUGuOxwCmOtjcghKK-dDRg&logo=false"
+      else
+        #use map center
+        map_url_end = "/"+all_longitudes.min.to_s+","+all_latitudes.min.to_s+",9,0/1024x576@2x?&access_token=pk.eyJ1IjoibWFyY3VzLWZvcmNobmVyIiwiYSI6ImNreDA1MHI2czBkMXUycXFsd2JqcWVtZWoifQ.LUGuOxwCmOtjcghKK-dDRg&logo=false"
+      end    
+
       counter = 0
       self.body.embeds.each do |embedded_image|
         counter = counter + 1
