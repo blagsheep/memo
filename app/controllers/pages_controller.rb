94bc6898a70b45695a20d6c8dd9af47367bbb432
Memo <marcus.forchner@gmail.com>
2022-02-11 22:06:51 +0000

    change article order to date desc

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
     #NotificationMailer.with(message: "My dear reader, this is a test!").notify_admin.deliver_later
     # avoid N+1 query: https://edgeguides.rubyonrails.org/action_text_overview.html#avoid-n-1-queries
     
-    @pagy, @posts = pagy(Post.all.with_rich_text_body_and_embeds.order(created_at: :desc))
+    @pagy, @posts = pagy(Post.all.with_rich_text_body_and_embeds.order(date: :desc))
 
     #@posts = Post.all.with_rich_text_body_and_embeds.order(created_at: :desc)
 
